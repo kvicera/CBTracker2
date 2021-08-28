6be04daff97e22f5aa97bd76fed4980d58733fc2
Kim Vicera <victorfrancis.vicera@bicol-u.edu.ph>
2021-08-28 11:31:44 +0800

    created test branch

--- App.js
+++ App.js
@@ -1,24 +1,43 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+//import {Button, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 
-function HomeScreen({ navigation }) {
+import {Component} from 'react';
+import {View, TextField, Text, Button} from 'react-native-ui-lib';
+
+import {StyleSheet, Alert, FlatList} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import {AnimatableManager, Colors, BorderRadiuses, ListItem, Text} from 'react-native-ui-lib';
+import orders, {OrderType} from '../../data/orders';
+
+function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
+    <View flex paddingH-25 paddingT-120>
+      <Text blue50 text20>
+        Welcome
+      </Text>
+      <TextField text50 placeholder="username" dark10 />
+      <TextField text50 placeholder="password" secureTextEntry dark10 />
+      <View marginT-100 center>
+        <Button text70 white background-orange30 label="Login" />
+        <Button link text70 orange30 label="Sign Up" marginT-20 />
+      </View>
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function NotificationsScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
+    <ListItem
+          // @ts-expect-error
+          activeBackgroundColor={Colors.dark60}
+          activeOpacity={0.3}
+          height={77.5}
+          onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
+        >
+
+    </ListItem>
   );
 }
 
